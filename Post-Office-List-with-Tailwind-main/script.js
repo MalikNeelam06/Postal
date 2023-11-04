6a0baf703447b176a2203f955d5e80a5cb2f23ed
Malik_Neelam06 <nmalik060191@gmail.com>
2023-11-04 20:07:06 +0530

    first commit

--- Post-Office-List-with-Tailwind-main/script.js
+++ Post-Office-List-with-Tailwind-main/script.js
@@ -14,8 +14,8 @@ async function getIP() {
     ipAddress = result.ip
     const getStartedButton = document.getElementById("getStarted");
     getStartedButton.addEventListener("click", () => {
-      document.cookie = `ip=${ipAddress}; path=https://github.com/MalikNeelam06/Postal/tree/main/Post-Office-List-with-Tailwind-main/locationDetails/main-page.html`;
-      window.location.href = "https://github.com/MalikNeelam06/Postal/tree/main/Post-Office-List-with-Tailwind-main/locationDetails/main-page.html";
+      document.cookie = `ip=${ipAddress}; path=https://MalikNeelam06.github.io/locationDetails/main-page.html`;
+      window.location.href = "https://MalikNeelam06.github.io/locationDetails/main-page.html";
     });
     console.log(ipAddress);
   } catch (error) {
